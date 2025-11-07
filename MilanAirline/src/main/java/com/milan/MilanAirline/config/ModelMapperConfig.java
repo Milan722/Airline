7dbc063f37bcd9d21543d03eafedd6669936d85f
MilanAnicic <anicicmilan72@gmail.com>
2025-11-07 18:34:04 +0100

    Revert Resend API changes - restore original SMTP implementation

--- MilanAirline/src/main/java/com/milan/MilanAirline/config/ModelMapperConfig.java
+++ MilanAirline/src/main/java/com/milan/MilanAirline/config/ModelMapperConfig.java
@@ -5,7 +5,6 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ModelMapperConfig {
@@ -19,10 +18,5 @@ public class ModelMapperConfig {
                 .setMatchingStrategy(MatchingStrategies.STANDARD);
 
         return modelMapper;
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
